1e95b83e98da80624a4ed4b94e91dda2f7b650f1
philippesaxer <philippe.saxer@edu.tbz.ch>
2025-12-08 10:11:39 +0100

    Clean up comments in loadAssets function
    Removed commented lines for clarity.

--- src/assets.js
+++ src/assets.js
@@ -1,13 +1,13 @@
 export async function loadAssets(THREE) {
-  // 1. Textur laden
+  
   const textureLoader = new THREE.TextureLoader();
-  // PASSEN SIE DEN DATEINAMEN HIER AN
+  
   const botTexture = await textureLoader.loadAsync('images/farhan.png'); 
 
-  // 2. MeshStandardMaterial erstellen und Textur als 'map' verwenden
+
   const botMaterial = new THREE.MeshStandardMaterial({
     map: botTexture,
-    // Optional: Eigenschaften, um die Textur besser zur Geltung zu bringen
+  
     roughness: 1, 
     metalness: 0 
   });

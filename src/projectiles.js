47f4317a4e215f665e2959a4ee12d9ac65abeab4
philippesaxer <philippe.saxer@edu.tbz.ch>
2026-01-19 09:22:01 +0100

    Change projectile rotation from x to z axis

--- src/projectiles.js
+++ src/projectiles.js
@@ -8,7 +8,7 @@ export class Projectile {
     const geo = new THREE.CylinderGeometry(0.05, 0.05, 0.4, 12);
     const mat = new THREE.MeshStandardMaterial({ color: 0xffffff, emissive: 0x222222 });
     this.mesh = new THREE.Mesh(geo, mat);
-    this.mesh.rotation.x = Math.PI / 2;
+    this.mesh.rotation.z = Math.PI / 2;
     this.mesh.visible = false;
     scene.add(this.mesh);
 
